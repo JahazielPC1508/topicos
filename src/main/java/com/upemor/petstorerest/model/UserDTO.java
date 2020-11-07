6c0b7cfb761edb0d3487806903e3678cb515bd5f
JahazielPC1508 <jahazielperalta01@gmail.com>
2020-11-06 18:13:50 -0600

    1.0

---
+++ src/main/java/com/upemor/petstorerest/model/UserDTO.java
@@ -0,0 +1,64 @@
+package com.upemor.petstorerest.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class UserDTO {
+	@Id
+	@GeneratedValue
+	private int id;
+	
+	private String username;
+	private String firstname;
+	private String lasttname;
+	private String email;
+	private String password;
+	private boolean status;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLasttname() {
+		return lasttname;
+	}
+	public void setLasttname(String lasttname) {
+		this.lasttname = lasttname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public boolean isStatus() {
+		return status;
+	}
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+	
+	
+	public UserDTO() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+	
+}
